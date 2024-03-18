9e35a68427c6e831bf3f8ddc48ba220b29573ed5
jek_a709 <evgenijblagun13@gmail.com>
2024-03-18 15:25:24 +0300

    upd

--- scripts.js
+++ scripts.js
@@ -1,4 +1,22 @@
+// function save(){
+//     text = document.getElementById("text").value;
+//     alert(text);
+//     cont = document.getElementsByClassName("container");
+//     cont.innerHtml + '<div class="element">Element</div>';
+//     cont.innerHtml + '<p>hello</p>';
+//     alert("greate");
+// }
+
+
 function save(){
     text = document.getElementById("text").value;
-    alert(text);
+    const header = document.createElement("div");        // создаем заголовок <h1>
+    header.textContent = text; // определяем текст элемента
+    header.className = "mark";
+    // получаем первый параграф
+    const place = document.getElementById("text");
+    // добавляем элемент h1 перед параграфом firstP
+    document.body.appendChild(header); 
 }
+
+
