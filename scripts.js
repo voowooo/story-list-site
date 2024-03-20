d6ba63a399b34eea297e6f128e9a5140197564e7
jek_a709 <evgenijblagun13@gmail.com>
2024-03-20 14:50:45 +0300

    upd

--- scripts.js
+++ scripts.js
@@ -3,10 +3,15 @@ function save(){
     const header = document.createElement("div");        // создаем заголовок <h1>
     header.textContent = text; // определяем текст элемента
     header.className = "mark";
-    // получаем первый параграф
-    const place = document.getElementById("text");
+    header.id = "mark";
     // добавляем элемент h1 перед параграфом firstP
-    document.body.appendChild(header); 
+    document.body.appendChild(header);
+
+    theme = document.getElementById("markTheme").value;
+    const voo = document.createElement("div");
+    voo.textContent = theme;
+    const placev = document.getElementById("text");
+    document.body.appendChild(voo);
 }
 
 // Выбираем кнопку
@@ -16,3 +21,4 @@ btn.addEventListener('click', function() {
   // Затем переключаем (добавляем/удаляем) класс .dark-theme для body
   document.body.classList.toggle('dark-theme'); 
 })
+
