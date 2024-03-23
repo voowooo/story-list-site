ba23371765a711dcde387c52d543c0a9fc666b85
jek_a709 <evgenijblagun13@gmail.com>
2024-03-23 12:06:50 +0300

    upd
    now theme saves to the local storage

--- scripts.js
+++ scripts.js
@@ -25,14 +25,6 @@ function save(){
     }
 }
 
-// Выбираем кнопку
-const btn = document.getElementById("modeT");
-// Отслеживаем щелчок по кнопке
-btn.addEventListener('click', function() {
-  // Затем переключаем (добавляем/удаляем) класс .dark-theme для body
-  document.body.classList.toggle('dark-theme'); 
-})
-
 function saveList(){
   localStorage.storedList = document.getElementById("place").innerHTML;
 }
@@ -59,3 +51,28 @@ function devMode(){
     clearButton.style.display = "";
 }
 
+
+// Выбираем кнопку
+const btn = document.getElementById("modeT");
+// Выбираем настройки темы из localStorage
+const currentTheme = localStorage.getItem("theme");
+// Если текущая тема в localStorage равна "dark"…
+if (currentTheme == "dark") {
+  // …тогда мы используем класс .dark-theme
+  document.body.classList.add("dark-theme");
+}
+
+// Отслеживаем щелчок по кнопке
+btn.addEventListener("click", function() {
+  // Переключаем класс .dark-theme при каждом щелчке
+  document.body.classList.toggle("dark-theme");
+  // Допустим, тема светлая
+  let theme = "light";
+  // Если <body> содержит класс .dark-theme…
+  if (document.body.classList.contains("dark-theme")) {
+    // …тогда делаем тему тёмной
+    theme = "dark";
+  }
+  // После чего сохраняем выбор в localStorage
+  localStorage.setItem("theme", theme);
+});
