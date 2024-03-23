0f61a76a122a0f2b4ab9ac0c5e71f8ce4272fb78
jek_a709 <evgenijblagun13@gmail.com>
2024-03-23 14:42:34 +0300

    upd

---
+++ lang.js
@@ -0,0 +1,30 @@
+const langArr = {
+    "unit" : {
+        "ru": "SL · главная",
+        "en": "SL · main",
+    },
+    "name" : {
+        "ru": "Заметки",
+        "en": "Notes"
+    },
+    "markTheme" : {
+        "ru": "тема",
+        "en": "theme",
+    },
+    "text" : {
+        "ru": "текст",
+        "en": "text",
+    },
+    "save" : {
+        "ru": "сохранить",
+        "en": "save",
+    },
+    "adText" : {
+        "ru": "Здесь может быть </br> ваша реклама <br> <br> а может и не быть <br> <br> я вообще хз",
+        "en": "There may be </br> your ad <br> <br> or not <br> <br> I don’t know at all",
+    },
+    "v" : {
+        "ru": "заметки:",
+        "en": "notes:",
+    },
+}
--- scripts.js
+++ scripts.js
@@ -53,7 +53,7 @@ function devMode(){
 
 
 // Выбираем кнопку
-const btn = document.getElementById("modeT");
+const btn = document.getElementById("togg");
 // Выбираем настройки темы из localStorage
 const currentTheme = localStorage.getItem("theme");
 // Если текущая тема в localStorage равна "dark"…
@@ -64,6 +64,7 @@ if (currentTheme == "dark") {
 
 // Отслеживаем щелчок по кнопке
 btn.addEventListener("click", function() {
+  console.log("cheked")
   // Переключаем класс .dark-theme при каждом щелчке
   document.body.classList.toggle("dark-theme");
   // Допустим, тема светлая
@@ -76,3 +77,53 @@ btn.addEventListener("click", function() {
   // После чего сохраняем выбор в localStorage
   localStorage.setItem("theme", theme);
 });
+
+
+
+
+
+
+
+const select = document.getElementById("select");
+const allLang = ['en', 'ru'];
+
+select.addEventListener('change', changeURL);
+
+function changeURL() {
+  let lang = select.value;
+  location.href = window.location.pathname + '#' + lang;
+  location.reload();
+}
+
+function changeLang(){
+  let hash = window.location.hash;
+  hash = hash.substr(1);
+  console.log(hash);
+  if (!allLang.includes(hash)){
+    location.href = window.location.pathname + '#en';
+    location.reload();
+  }
+  select.value = hash;
+  document.querySelector('title').innerHTML = langArr['unit'][hash];
+  document.getElementById('vop').innerHTML = langArr['name'][hash];
+  document.getElementById('markTheme').placeholder = langArr['markTheme'][hash];
+  document.getElementById('text').placeholder = langArr['text'][hash];
+  document.getElementById('save').innerHTML = langArr['save'][hash];
+  document.getElementById('adText').innerHTML = langArr['adText'][hash];
+  document.getElementById('v').innerHTML = langArr['v'][hash];
+}
+
+changeLang();
+
+var toggleSettings = 0;
+
+function settings() {
+  const settings = document.getElementById("settings");
+  if (toggleSettings == 0){
+    settings.style.display = "flex";
+    toggleSettings = 1;
+  } else if (toggleSettings == 1){
+    settings.style.display = "none";
+    toggleSettings = 0;
+  }
+}
